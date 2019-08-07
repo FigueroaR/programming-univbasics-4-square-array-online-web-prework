1120972d40024f135925c55fcfbcf978158aabf3
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-08-07 23:28:37 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
-  # your code here
+
+
+numbers = [1,2,3]
+
+def square_array(numbers > 0)
+  while numbers > 0
+  Math.sqrt(numbers)
+  puts numbers[]
+  
+# => [1,4,9])
+end 
+
+
+new_numbers = [9,10,16,25]
+
+while square_array(new_numbers)
+  Math.sqrt(numbers)
+# => [81,100,256,625]
 end
